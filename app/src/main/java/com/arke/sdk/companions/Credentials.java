b36ec4fdeecbe9380209d6e103c0d463419eee71
sanxy <yusufadefolahan@gmail.com>
2019-10-22 17:59:20 +0100

    changed server to back4app

--- app/src/main/java/com/arke/sdk/companions/Credentials.java
+++ app/src/main/java/com/arke/sdk/companions/Credentials.java
@@ -1,9 +1,14 @@
 package com.arke.sdk.companions;
 
 public class Credentials {
-    public static String PARSE_APPLICATION_ID = "YfXzE8r7WP9zgsr1g521Yv9OO875RAUQkfUE4RUT";
-    public static String PARSE_CLIENT_KEY = "L71JZ2lQKdym79GdnJ80b4mHb5RlkTRU87OqOQm4";
-    public static String PARSE_SERVER_URL = "https://pg-app-dpuwbpezm43ehrd49quy2rej9z6zx2.scalabl.cloud/1/";
+    // satoshi
+//    public static String PARSE_APPLICATION_ID = "YfXzE8r7WP9zgsr1g521Yv9OO875RAUQkfUE4RUT";
+//    public static String PARSE_CLIENT_KEY = "L71JZ2lQKdym79GdnJ80b4mHb5RlkTRU87OqOQm4";
+//    public static String PARSE_SERVER_URL = "https://pg-app-dpuwbpezm43ehrd49quy2rej9z6zx2.scalabl.cloud/1/";
+    // Back4app
+public static String PARSE_APPLICATION_ID = "pe8tJoY2mN6qdz35FSLuVpJKAGlh6QKPyv0ZuQP8";
+    public static String PARSE_CLIENT_KEY = "kvPfkn0tpMSj2pBhZGKMF1NyMCuqwXUeSUKNpLNS";
+    public static String PARSE_SERVER_URL = "https://parseapi.back4app.com/";
     public static String FILE_STACK_API_KEY = "Ae6IsVjvQLxfJlUV12rwZz";
     public static String FILE_STACK_SECURITY_KEY = "QNTQDWQSJZHODGIJYQ7QORY5PU";
     public static String SEND_GRID_API_KEY = "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";
